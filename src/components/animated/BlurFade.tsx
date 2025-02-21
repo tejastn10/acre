eec56535a1dc90569fdf5804c7f30ba0e821cd5d
Tejas Nikhar <tejastn10@gmail.com>
2025-02-21 07:17:10 +0530

    feat: add BlurFade component for animated blur and fade effects

---
+++ src/components/animated/BlurFade.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import { FC, ReactNode, useRef } from "react";
+
+import { AnimatePresence, useInView, Variants } from "motion/react";
+
+import { motion } from "motion/react";
+
+type BlurFadeProps = {
+	blur?: string;
+	delay?: number;
+	yOffset?: number;
+	inView?: boolean;
+	duration?: number;
+	inViewMargin?: string;
+	variant?: Variants;
+	className?: string;
+	children: ReactNode;
+};
+
+const BlurFade: FC<BlurFadeProps> = ({
+	variant,
+	children,
+	className,
+	delay = 0,
+	yOffset = 6,
+	blur = "6px",
+	duration = 0.4,
+	inView = false,
+	inViewMargin = "-50px",
+}) => {
+	const ref = useRef<HTMLDivElement | null>(null);
+
+	const inViewResult = useInView(ref as React.RefObject<Element>, {
+		once: true,
+		margin: inViewMargin as any,
+	});
+	const isInView = !inView || inViewResult;
+
+	const defaultVariants: Variants = {
+		hidden: { y: yOffset, opacity: 0, filter: `blur(${blur})` },
+		visible: { y: -yOffset, opacity: 1, filter: "blur(0px)" },
+	};
+	const combinedVariants = variant || defaultVariants;
+
+	return (
+		<AnimatePresence>
+			<motion.div
+				ref={ref}
+				exit="hidden"
+				initial="hidden"
+				variants={combinedVariants}
+				animate={isInView ? "visible" : "hidden"}
+				transition={{
+					duration,
+					ease: "easeOut",
+					delay: 0.04 + delay,
+				}}
+				className={className}
+			>
+				{children}
+			</motion.div>
+		</AnimatePresence>
+	);
+};
+
+export { BlurFade };
