ed4499e3225a6b49b312e9069a91598c46a51463
poojaghodmode <poojaghodmode@gmail.com>
2025-03-08 14:41:43 +0530

    fix: formatting of indian currency

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -121,7 +121,7 @@ export default function ReturnCalculator() {
 	const formatCurrency = (value?: number) => {
 		if (value === undefined) return "₹0";
 
-		return new Intl.NumberFormat("en-US", {
+		return new Intl.NumberFormat("en-IN", {
 			style: "currency",
 			currency: "INR",
 			maximumFractionDigits: 0,
